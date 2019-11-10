2030646eac9375b92066f8304afe7e0a9dd37fae
Fabrizio Giordano <giordano.fabrizio@gmail.com>
2019-11-09 23:48:39 -0500

    Add post

--- content/blog/hello-world/index.md
+++ content/blog/hello-world/index.md
@@ -1,10 +1,10 @@
 ---
 title: Hello World
-date: "2015-05-01T22:12:03.284Z"
+date: "2019-11-09"
 description: "Hello World"
 ---
 
-This is my first post on my new fake blog! How exciting!
+First post
 
 I'm sure I'll write a lot more interesting things in the future.
 
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `Fabrizio Giordano`,
+    author: `Fabrizio Giordano`,
+    description: `My thoughts`,
+    siteUrl: `https://www.fabriziogiordano.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `fabrygio`,
     },
   },
   plugins: [
@@ -57,13 +57,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Fabrizio Giordano Blog`,
+        short_name: `fabrygio-blog`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/icon.png`,
       },
     },
     `gatsby-plugin-offline`,
