29146b68d4348d859f883b00e104172fe7a56e39
Fabrizio Giordano <giordano.fabrizio@gmail.com>
2019-11-11 23:16:37 -0500

    add gif and update bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -54,11 +54,10 @@ const Bio = () => {
         }}
       />
       <p>
-        Written by <strong>{author}</strong> who lives and works in San
-        Francisco building useful things.
+        Written by <strong>{author}</strong> who lives and works in New York City building useful things.
         {` `}
         <a href={`https://twitter.com/${social.twitter}`}>
-          You should follow him on Twitter
+          You should follow me on Twitter
         </a>
       </p>
     </div>
