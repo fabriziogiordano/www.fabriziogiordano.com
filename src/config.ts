5f6b25253d96a8cc0b5ae8f684f8fa4fa851a472
Fabrizio Giordano <giordano.fabrizio@gmail.com>
2022-10-23 04:00:20 -0400

    tweack design

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Fabrizio`s personal web site';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Fabrizio Giordano Blog';
+export const SITE_DESCRIPTION = 'Welcome to my blog!';
