10b3196c072c1cdc3cd248d0cd2aa6217f44d636
Fabrizio Giordano <giordano.fabrizio@gmail.com>
2022-10-23 04:09:13 -0400

    compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,22 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
-import sitemap from '@astrojs/sitemap';
+// https://astro.build/config
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://example.com",
+  integrations: [
+    mdx(),
+    sitemap(),
+    compress({
+      css: true,
+      html: true,
+      img: false,
+      js: true,
+      svg: true,
+    }),
+  ],
 });
