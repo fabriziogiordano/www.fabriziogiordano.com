9e8c255d8ba23f18bdc2d33a87e56845a63067a3
Fabrizio Giordano <giordano.fabrizio@gmail.com>
2023-02-19 21:16:17 +0000

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,15 +7,52 @@ import compress from "astro-compress";
 
 export default defineConfig({
   site: "https://www.fabriziogiordano.com",
+  base: "/",
+  trailingSlash: "never",
+  output: "static",
+  outDir: "./dist",
+
+  server: {
+    port: 4050
+  },
+
+  build: {
+    format: "files",
+    assets: "assets"
+  },
+
+  vite: {
+    resolve: {
+      alias: {
+        "~": path.resolve(__dirname, "./src")
+      }
+    },
+    server: {
+      watch: {
+        ignored: ["/dist/**", "/node_modules/**", "/.astro/**", "astro.log"]
+      }
+    },    
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: 'main.[hash].js',
+          chunkFileNames: 'chunks/chunk.[hash].js',
+          assetFileNames: 'assets/asset.[hash][extname]',
+        },
+      },
+    },
+  },
+
   integrations: [
     mdx(),
     sitemap(),
     compress({
+      outDir: "./dist",
       css: true,
       html: true,
       img: false,
       js: true,
-      svg: true,
+      svg: false,
     }),
   ],
 });
