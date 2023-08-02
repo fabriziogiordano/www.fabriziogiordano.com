d90759fbce45334cf4d99c82c234e3430a742f40
Fabrizio Giordano <giordano.fabrizio@gmail.com>
2023-08-02 04:02:11 +0000

    new deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
-revised=$(date +"%m-%d-%y")
-echo $revised
-sed -ri "s/(<meta name=\"revised\" content=\")(.*)(\" \/>)/\1$revised\3/" ./src/components/BaseHead.astro
+REVISITED=$(date +"%m-%d-%y")
+echo $REVISITED
+sed -ri "s/(<meta name=\"revised\" content=\")(.*)(\" \/>)/\1$REVISITED\3/" ./src/components/BaseHead.astro
+
 git pull
 git add ./src/components/BaseHead.astro
-git commit -m"BaseHead revised to $revised"
+git commit -m"BaseHead revised to $REVISITED"
 git push
